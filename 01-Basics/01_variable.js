7e2df5190a363c6fe645e281ddf5b08f73d9f2b7
Tehreem-Naseer <57981857+Tehreem-Naseer@users.noreply.github.com>
2024-05-08 05:10:20 +0000

    complete 01 variable

--- 01-Basics/01_variable.js
+++ 01-Basics/01_variable.js
@@ -1,7 +1,15 @@
 const accountId = 1234;
+
+// we can declare variable using 2 ways let and var but we mostly do not use var variable because it have scope issue
+// in var scope is not defined 
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
 let accountEmail = "tehreennaseer@gmail.com"
 var accountPassword = "tehreem519"
 accountCity = "Lahore"
+let accountState;  // its value print as undefine
 
 //accountId = 98227;  // cannot change value of constant variable  i tried and it give following error not allowed to change its value
 
@@ -12,6 +20,6 @@ accountPassword = "abc6789"
 accountCity  = "Islamabad"
 
 console.log(accountId);
-console.table([accountId ,accountEmail ,accountPassword,accountCity]); // console.table is used to print more than one variable
+console.table([accountId ,accountEmail ,accountPassword,accountCity , accountState]); // console.table is used to print more than one variable
 
 
