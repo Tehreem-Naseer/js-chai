1424597c786d600d8649f9d918e9b5f8cf782f88
Tehreem-Naseer <57981857+Tehreem-Naseer@users.noreply.github.com>
2024-05-08 06:54:18 +0000

    dataType complete

---
+++ 01-Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat js code as a newer version of js 
+
+//we can run it in browser in node it have different syntax
+//alert(3+3)  
+
+//console.log(3+3)  console.log("Tehreem") //give error when use side by side also it decreae readabilty
+
+//mdn is documentation by mozila fox
+//ecma script is its original documentation
+
+let name = "tehreem"  //string
+let age = 18   // number
+let isLoggedIn = false   //boolean
+
+
+//number
+//bigint
+//string
+//boolean 
+//null      standalone value  intentionally defined
+//undefined  when we declare variable but didn't assign any value to it
+//symbol   for define uniqueness in figma and react
+
+//object
+
+console.log(typeof "tehreem")    // to check the datatype of variablee
+console.log(typeof age)
+console.log(typeof null)   // it will return object 
+console.log(typeof undefined) // it will return undefined because its a type in js
