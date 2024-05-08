609e8043c4c00466e8075e538b4fe31a4033b8ec
Tehreem-Naseer <57981857+Tehreem-Naseer@users.noreply.github.com>
2024-05-08 08:08:00 +0000

    conversionOperation concatination etc

---
+++ 01-Basics/04_conversionOperation.js
@@ -0,0 +1,39 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+console.log(2+2)  //addition
+console.log(2-2)  //subtraction
+console.log(2*2)  //multiplication
+console.log(2**3) //power
+console.log(2/3)  //divide
+console.log(2%3)  //module(remainder)
+
+let str = "Hello"
+let str1 = "Tehreem"
+
+let str2 = str + str1
+
+console.log(str2)  //return HelloTehreem 
+console.log("1" + 2) //return 12
+console.log(1 + "2") //return 12
+console.log("1" + 2 + 2) //return 122 
+console.log(1 + 2 + "2") //return 32
+
+console.log(true)    // return true
+console.log(+true)   //return 1
+//console.log(true+) // return error
+console.log(+"") //return 0
+
+let num1, num2, num3 
+ num1 = num2 = num3 = 2+2
+
+ console.table([num1 , num2 , num3])
+
+ let gameCounter = 0
+ console.log(gameCounter++)  //return 0
+ console.log(++gameCounter)  // return 2
+
+ // link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
